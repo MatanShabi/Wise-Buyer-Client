df90c23e59097b50589476d3e7f8dbb63a195a54
MatanShabi <46000126+MatanShabi@users.noreply.github.com>
2024-01-25 08:04:27 +0200

    add validations to login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button, Container, TextField, Typography, Paper } from '@mui/material';
 import { useForm, Controller } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 
 interface LoginForm {
   email: string;
@@ -8,15 +9,20 @@ interface LoginForm {
 }
 
 const Login: React.FC = () => {
-  const { control, handleSubmit } = useForm<LoginForm>();
+  const navigate = useNavigate();
+  const { control, handleSubmit, formState } = useForm<LoginForm>();
 
   const handleLogin = (data: LoginForm) => {
     console.log('Login data:', data);
   };
 
+  const handleSignup = () => {
+    navigate('/signup')
+  }
+
   return (
-    <Container  maxWidth="xs">
-    <Paper elevation={3} sx={{ padding: 10, display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: 8 }}>
+    <Container maxWidth="xs">
+    <Paper elevation={3} sx={{ padding: 10, display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: 20 }}>
         <img src='src/assets/logo.svg' alt='Logo' className='w-28' />      
       <Typography component="h1" variant="h5" gutterBottom>
         Wise Buyer
@@ -26,12 +32,20 @@ const Login: React.FC = () => {
           name="email"
           control={control}
           defaultValue=""
+          rules={{
+            required: 'Email is required',
+            pattern: {
+              value: /\S+@\S+\.\S+/,
+              message: 'Invalid email address',
+            },
+          }}
           render={({ field }) => (
             <TextField
               label="Email"
               variant="outlined"
               fullWidth
               margin="normal"
+              error={!!formState.errors.email}
               {...field}
             />
           )}
@@ -40,6 +54,9 @@ const Login: React.FC = () => {
           name="password"
           control={control}
           defaultValue=""
+          rules={{
+            required: 'Password is required',
+          }}
           render={({ field }) => (
             <TextField
               label="Password"
@@ -47,13 +64,19 @@ const Login: React.FC = () => {
               variant="outlined"
               fullWidth
               margin="normal"
+              error={!!formState.errors.password}
               {...field}
             />
           )}
         />
+        <div className='flex flex-row gap-1'>
         <Button type="submit" variant="contained" color="primary" fullWidth sx={{ marginTop: 2 }}>
-          Sign In
+          Login
         </Button>
+        <Button onClick={handleSignup} type="submit" variant="contained" color="secondary" fullWidth sx={{ marginTop: 2 }}>
+          Sign Up
+        </Button>
+        </div>
       </form>
     </Paper>
   </Container>
