f545b6d0f56fa5e552e4715a6b9997f28c6ac0c7
Raz-Hagay-Gavriel <raz5338@gmail.com>
2024-02-07 04:56:02 +0200

    Add getUserPosts function to fetch user-specific posts

--- src/api/post.ts
+++ src/api/post.ts
@@ -27,3 +27,20 @@ export const createPost = async (postData: IPost) => {
         console.error("Error fetching posts:", error);
     }
 };
+
+export const getUserPosts = async () => {
+    try {        
+        const token = getAuthToken();
+        let userid = token?.split('.')[1];
+        userid = userid ? JSON.parse(atob(userid))._id : undefined;
+        console.log(userid);        
+
+        return apiClient.get(`/post/user/${userid}`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+    } catch (error) {
+        console.error("Error fetching posts:", error);
+    }
+};
