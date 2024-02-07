811bf8346f0caef25ff1071842d6a3ecab0a9a35
Raz-Hagay-Gavriel <raz5338@gmail.com>
2024-02-07 14:23:24 +0200

    add icon to navbar + change application div to be bold

--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography} from '@mui/material';
-import { Link } from 'react-router-dom';
-import useUser from '../../hooks/useUser';
-import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import React from "react";
+import { AppBar, Toolbar, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import useUser from "../../hooks/useUser";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
 import IconButton from "@mui/material/IconButton";
-import InfoIcon from '@mui/icons-material/Info';
-import PolicyIcon from '@mui/icons-material/Policy';
-import LogoutIcon from '@mui/icons-material/Logout';
+import InfoIcon from "@mui/icons-material/Info";
+import PolicyIcon from "@mui/icons-material/Policy";
+import LogoutIcon from "@mui/icons-material/Logout";
 
-interface NavbarProps { }
+interface NavbarProps {}
 
 const Navbar: React.FC<NavbarProps> = () => {
-  const { logoutUser } = useUser()
-  
+  const { logoutUser } = useUser();
+
   return (
     <AppBar position="static">
-      <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
-        <Typography variant="h6" component={Link} to="/">
-          Wise-Buyer
+      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+        <Typography
+          variant="h6"
+          component={Link}
+          to="/"
+          sx={{ display: "flex", alignItems: "center" }}
+        >
+          <img
+            src="src/assets/logo.svg"
+            style={{ width: "80px", height: "80px", marginRight: "5px" }}
+          />
+          <p style={{ fontWeight: 'bold', fontSize: '20px' }}>Wise-Buyer</p>
         </Typography>
-        <div>
 
+        <div>
           <IconButton color="inherit" component={Link} to="/profile">
             <AccountBoxIcon />
             <p>Profile</p>
@@ -31,21 +40,23 @@ const Navbar: React.FC<NavbarProps> = () => {
             <p>About Us</p>
           </IconButton>
 
-          
           <IconButton color="inherit" component={Link} to="/privacypolicy">
             <PolicyIcon />
             <p>Privacy Policy</p>
           </IconButton>
-
         </div>
-        <IconButton onClick={logoutUser} color="inherit" component={Link} to="/">
-            <LogoutIcon />
-            <p>Logout</p>
-          </IconButton>
-
+        <IconButton
+          onClick={logoutUser}
+          color="inherit"
+          component={Link}
+          to="/"
+        >
+          <LogoutIcon />
+          <p>Logout</p>
+        </IconButton>
       </Toolbar>
     </AppBar>
   );
-}
+};
 
 export default Navbar;
