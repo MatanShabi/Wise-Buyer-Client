b67b1d7ad06fc3778ace4b817a8d81bcf497c35e
Raz-Hagay-Gavriel <raz5338@gmail.com>
2024-02-08 21:01:29 +0200

    Add HTTPS configuration for server

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import fs from 'fs';
 
 export default defineConfig({
+  server: {
+    https: {
+      key: fs.readFileSync('../Wise-Buyer-Server/client-key.pem'),
+      cert: fs.readFileSync('../Wise-Buyer-Server/client-cert.pem'),
+    },
+  },
   plugins: [react()],
-})
+});
