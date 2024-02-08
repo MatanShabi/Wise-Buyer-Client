3d6d5e6da10d5c72b7017a647171812a05078245
Raz-Hagay-Gavriel <raz5338@gmail.com>
2024-02-08 04:34:02 +0200

    Refactor about us and privacy policy pages layout and styles

--- src/components/aboutus/index.tsx
+++ src/components/aboutus/index.tsx
@@ -1,107 +1,90 @@
-import {
-  Container,
-  Typography,
-  Box,
-  Paper,
-  Grid,
-  Avatar,
-  Card,
-  CardContent,
-} from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-  },
-  avatar: {
-    margin: "auto",
-    width: theme.spacing(7),
-    height: theme.spacing(7),
-  },
-  card: {
-    marginTop: theme.spacing(3),
-    padding: theme.spacing(3),
-    maxWidth: "80%",
-    minHeight: "70vh",
-    margin: "auto",
-  },
-}));
-
-const teamMembers = [
-  {
-    name: "Raz Hagay Gavriel",
-    imageUrl: "http://localhost:3000/public/TeamMembers/raz.jpeg",
-    phoneNumber: "(+972)50-960-5540",
-  },
-  {
-    name: "Matan Shabi",
-    imageUrl: "http://localhost:3000/public/TeamMembers/matan.jpeg",
-    phoneNumber: "(+972)54-831-5013",
-  },
-];
+import { Container, Typography, Grid, Avatar } from "@material-ui/core";
 
 const AboutUsPage = () => {
-  const classes = useStyles();
+  const teamMembers = [
+    {
+      name: "Raz Hagay Gavriel",
+      imageUrl: "http://localhost:3000/public/TeamMembers/raz.jpeg",
+      phoneNumber: "(+972)50-960-5540",
+    },
+    {
+      name: "Matan Shabi",
+      imageUrl: "http://localhost:3000/public/TeamMembers/matan.jpeg",
+      phoneNumber: "(+972)54-831-5013",
+    },
+  ];
 
   return (
-    <Container maxWidth="lg" className={classes.root}>
-      <Card className={classes.card}>
-        <CardContent>
-          <Typography variant="h2" component="h1" gutterBottom align="center">
-            About Us
-          </Typography>
+    <Container maxWidth="lg" className="my-8">
+      <div
+        style={{
+          border: "2px solid #ccc",
+          padding: "50px",
+          width: "80%",
+          margin: "auto",
+        }}
+      >
+        <Typography variant="h2" component="h1" gutterBottom align="center">
+          About Us
+        </Typography>
 
-          <Typography
-            variant="body1"
-            gutterBottom
-            style={{ fontSize: "1.3rem" }}
-          >
-            This is a social media app that allows users to share and compare
-            the products they buy. Users can post about their latest purchases,
-            comment on others' posts, and engage in discussions about products.
-            <br />
-            <br />
-            This app was created by a team of developers who wanted to create a
-            platform where people can share their shopping experiences and
-            discover new products. We hope you enjoy using our app and we are
-            always looking for ways to improve it.
-          </Typography>
-          <Typography
-            variant="h4"
-            component="h2"
-            gutterBottom
-            align="center"
-            style={{ paddingBottom: "20px", paddingTop: "20px" }}
-          >
-            Team Members
-          </Typography>
+        <Typography variant="body1" gutterBottom style={{ fontSize: "1.3rem" }}>
+          This is a social media app that allows users to share and compare the
+          products they buy. Users can post about their latest purchases,
+          comment on others' posts, and engage in discussions about products.
+          <br />
+          <br />
+          This app was created by a team of developers who wanted to create a
+          platform where people can share their shopping experiences and
+          discover new products. We hope you enjoy using our app and we are
+          always looking for ways to improve it.
+        </Typography>
 
-          <Grid container spacing={2}>
-            {teamMembers.map((member, index) => (
-              <Grid item xs={8} sm={6} key={index}>
-                <Paper className={classes.paper}>
-                  <Avatar className={classes.avatar} src={member.imageUrl}>
-                    {member.name[0]}
-                  </Avatar>
-                  <Typography variant="h4" gutterBottom>
-                    {member.name}
-                  </Typography>
-                  <Typography variant="h5" gutterBottom>
-                    {member.phoneNumber}
-                  </Typography>
-                </Paper>
-              </Grid>
-            ))}
-          </Grid>
-        </CardContent>
-      </Card>
+        <Typography
+          variant="h4"
+          component="h2"
+          gutterBottom
+          align="center"
+          style={{ paddingBottom: "20px", paddingTop: "20px" }}
+        >
+          Team Members
+        </Typography>
+
+        <Grid container spacing={2} justifyContent="center">
+          {teamMembers.map((member, index) => (
+            <Grid
+              item
+              xs={8}
+              sm={5}
+              key={index}
+              style={{
+                border: "1px solid #ccc",
+                borderRadius: "5px",
+                margin: "10px",
+              }}
+            >
+              <div style={{ textAlign: "center" }}>
+                <Avatar
+                  src={member.imageUrl}
+                  style={{
+                    width: "100px",
+                    height: "100px",
+                    margin: "auto",
+                  }}
+                >
+                  {member.name[0]}
+                </Avatar>
+                <Typography variant="h4" gutterBottom>
+                  {member.name}
+                </Typography>
+                <Typography variant="h5" gutterBottom>
+                  {member.phoneNumber}
+                </Typography>
+              </div>
+            </Grid>
+          ))}
+        </Grid>
+      </div>
     </Container>
   );
 };
--- src/components/privacypolicy/index.tsx
+++ src/components/privacypolicy/index.tsx
@@ -1,12 +1,7 @@
 import { Container, Typography, Card, CardContent } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
-// Define styles for the component
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-  },
   card: {
     marginTop: theme.spacing(3),
     padding: theme.spacing(3),
@@ -16,102 +11,103 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-// Privacy Policy Page component
 const PrivacyPolicyPage = () => {
   const classes = useStyles();
 
   return (
-    <Container maxWidth="lg" className={classes.root}>
-      <Card className={classes.card}>
-        <CardContent>
-          <Typography variant="h2" component="h1" gutterBottom align="center">
-            Privacy Policy
-          </Typography>
+    <Container maxWidth="lg" className="my-8">
+      <div
+        style={{
+          border: "2px solid #ccc",
+          padding: "50px",
+          width: "80%",
+          margin: "auto",
+        }}
+      >
+        <Typography variant="h2" component="h1" gutterBottom align="center">
+          Privacy Policy
+        </Typography>
 
-          <Typography
-            variant="h5"
-            component="h2"
-            gutterBottom
-            style={{ paddingTop: "20px" }}
-          >
-            1. Introduction
-          </Typography>
-          <Typography
-            variant="body1"
-            gutterBottom
-            style={{ fontSize: "1.2rem" }}
-          >
-            Our app respects your privacy and is committed to protecting your
-            personal data. This privacy policy will inform you as to how we look
-            after your personal data when you visit our app (regardless of where
-            you visit it from) and tell you about your privacy rights and how
-            the law protects you.
-          </Typography>
+        <Typography
+          variant="h5"
+          component="h2"
+          gutterBottom
+          style={{ paddingTop: "20px" }}
+        >
+          1. Introduction
+        </Typography>
+        <Typography
+          variant="body1"
+          gutterBottom
+          style={{ fontSize: "1.2rem" }}
+        >
+          Our app respects your privacy and is committed to protecting your
+          personal data. This privacy policy will inform you as to how we look
+          after your personal data when you visit our app (regardless of where
+          you visit it from) and tell you about your privacy rights and how
+          the law protects you.
+        </Typography>
 
-          <Typography
-            variant="h5"
-            component="h2"
-            gutterBottom
-            style={{ paddingTop: "20px" }}
-          >
-            2. The data we collect about you
-          </Typography>
-          <Typography
-            variant="body1"
-            gutterBottom
-            style={{ fontSize: "1.2rem" }}
-          >
-            Personal data, or personal information, means any information about
-            an individual from which that person can be identified. It does not
-            include data where the identity has been removed (anonymous data).
-            <br></br>We may collect, use, store and transfer different kinds of
-            personal data about you which we have grouped together as follows:
-            <br></br>
-            <br />
-            <ul style={{ listStyleType: "disc", marginLeft: "20px" }}>
-              <li>
-                Identity Data includes [first name, last name or
-                similar identifier].
-              </li>
-              <li>
-                Contact Data includes [email address].
-              </li>
-              <li>
-                Financial Data includes [prices of your purchased items].
-              </li>
-              <li>
-                Transaction Data includes [details about payments to and from
-                you and other details of products and services you have
-                purchased from us].
-              </li>
-              <li>
-                Technical Data includes [internet protocol (IP) address, your
-                login data, browser type and version, time zone setting and
-                location, browser plug-in types and versions, operating system
-                and platform, and other technology on the devices you use to
-                access this app].
-              </li>
-              <li>
-                Profile Data includes [your username and password, purchases or
-                orders made by you, your interests, preferences, feedback and
-                survey responses].
-              </li>
-              <li>
-                Usage Data includes [information about how you use our app,
-                products and services].
-              </li>
-              <li>
-                Marketing and Communications Data includes [your preferences in
-                receiving marketing from us and our third parties and your
-                communication preferences].
-              </li>
-            </ul>
-          </Typography>
+        <Typography
+          variant="h5"
+          component="h2"
+          gutterBottom
+          style={{ paddingTop: "20px" }}
+        >
+          2. The data we collect about you
+        </Typography>
+        <Typography
+          variant="body1"
+          gutterBottom
+          style={{ fontSize: "1.2rem" }}
+        >
+          Personal data, or personal information, means any information about
+          an individual from which that person can be identified. It does not
+          include data where the identity has been removed (anonymous data).
+          <br></br>We may collect, use, store and transfer different kinds of
+          personal data about you which we have grouped together as follows:
+          <br></br>
+          <br />
+          <ul style={{ listStyleType: "disc", marginLeft: "20px" }}>
+            <li>
+              Identity Data includes [first name, last name or similar
+              identifier].
+            </li>
+            <li>Contact Data includes [email address].</li>
+            <li>Financial Data includes [prices of your purchased items].</li>
+            <li>
+              Transaction Data includes [details about payments to and from
+              you and other details of products and services you have
+              purchased from us].
+            </li>
+            <li>
+              Technical Data includes [internet protocol (IP) address, your
+              login data, browser type and version, time zone setting and
+              location, browser plug-in types and versions, operating system
+              and platform, and other technology on the devices you use to
+              access this app].
+            </li>
+            <li>
+              Profile Data includes [your username and password, purchases or
+              orders made by you, your interests, preferences, feedback and
+              survey responses].
+            </li>
+            <li>
+              Usage Data includes [information about how you use our app,
+              products and services].
+            </li>
+            <li>
+              Marketing and Communications Data includes [your preferences in
+              receiving marketing from us and our third parties and your
+              communication preferences].
+            </li>
+          </ul>
+        </Typography>
 
-          {/* Add more sections as needed */}
-        </CardContent>
-      </Card>
+        {/* Add more sections as needed */}
+      </div>
     </Container>
   );
 };
+
 export default PrivacyPolicyPage;
